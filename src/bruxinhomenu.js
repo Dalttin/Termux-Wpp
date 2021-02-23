a24617fe0d51b6fe506c463545de75fed334acc6
Seboladll <boulevard916@gmail.com>
2021-02-23 16:32:55 -0300

    First Commit

---
+++ src/bruxinhomenu.js
@@ -0,0 +1,12 @@
+const bruxinhomenu = (prefix, pushname) => {
+    return `◪ *Comandos do Sebola*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.bruxinhomenu = bruxinho
